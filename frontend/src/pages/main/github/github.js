146d7aee70f51820e80fdc5cebcfc29c9f2a760f
Mirco Palese <mircopalese@gmail.com>
2023-11-30 00:35:16 +0100

    removed electron app

--- frontend/src/pages/main/github/github.js
+++
@@ -1,7 +0,0 @@
-const githubSupport = document.querySelector('#github-support');
-githubSupport.addEventListener('click', e => {
-  e.preventDefault();
-  window.api.openExternalLink(
-    'https://github.com/MPalix86/programmatically_upload_csv_in_superset'
-  );
-});
--- frontend/src/pages/main/main.js
+++
@@ -1,9 +0,0 @@
-import { frontEndCommons, classes } from '../commons/js/global.js';
-
-const Paths = classes.Paths;
-
-console.log(Paths);
-
-const main = async function () {};
-
-main();
